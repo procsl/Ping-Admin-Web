e7fc5dec9848096533a2508fbda22c1e6d4eefe0
csl <program.chen@foxmail.com>
2020-06-23 08:22:03 +0800

    添加typescript,craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {FC} from 'react';
 import { Button } from 'antd';
 
-import './App.css';
+import './App.less';
 
 const App: FC = () => (
+
   <div className="App">
     <Button type="primary">Hello world</Button>
   </div>
